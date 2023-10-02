ab34ff6423551e21d90f38ab0a8abb0854e8e6ab
adarsh-1998-proj <64652298+adarsh-1998-proj@users.noreply.github.com>
2023-10-02 20:11:46 +0530

    addded
    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
+  # Restful Routes, only: [:show, :index, :new, :create, :edit, :update,]
   # Defines the root path route ("/")
   # root "articles#index"
 end
